278dc72f58bab99b4de4aa82edbafecee50152dc
bingoohuang <bingoo.huang@gmail.com>
2022-04-06 11:35:19 +0800

    refactor code

--- cmd/conn.go
+++ cmd/conn.go
@@ -11,13 +11,12 @@ import (
 )
 
 func newGRPCConnection(ctx context.Context, addr string, insecureOption bool) (*grpc.ClientConn, error) {
-	var dialOpts []grpc.DialOption
+	var t grpc.DialOption
 	if insecureOption {
-		dialOpts = append(dialOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		t = grpc.WithTransportCredentials(insecure.NewCredentials())
 	} else {
-		dialOpts = append(dialOpts, grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, "")))
+		t = grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, ""))
 	}
 
-	dialOpts = append(dialOpts, grpc.WithBlock(), grpc.WithBackoffMaxDelay(time.Second))
-	return grpc.DialContext(ctx, addr, dialOpts...)
+	return grpc.DialContext(ctx, addr, t, grpc.WithBlock(), grpc.WithBackoffMaxDelay(time.Second))
 }
